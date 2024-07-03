9c1f8c7e9bf8086abd218db82b664a92b3da0dea
Randy Miller <randy918@gmail.com>
2024-07-03 15:51:44 -0500

    start of Array Cardio Day 1 7-03-24-1551

--- js/index.js
+++ js/index.js
@@ -1,20 +1,11 @@
 
 //_ ████████████████████████████████████  FUNCTIONS
 
-function handleUpdate()  {
-const suffix = this.dataset.sizing || '';
-document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-
-
-}
 
 //_$ ████████████████████████████████████  QUERY SELECTORS
 
-const inputs = document.querySelectorAll('.controls input');
 
 //^ ████████████████████████████████████  EVENT LISTENERS
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
 //>  ████████████████████████████████████  REAL PROGRAM
